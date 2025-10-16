3fe96c92c60ad2a4d1c506241a12380584a82a1f
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2025-10-15 23:25:24 -0500

    updated api call function

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const colorBtn = document.getElementById("get-color")
+const form = document.getElementById("color-form")
 
-function generateColorScheme(){
-    fetch("https://www.thecolorapi.com/scheme?hex=24B1E0&mode=triad&count=6")
+function generateColorScheme(e){
+    e.preventDefault()
+    const formData = new FormData(form)
+    const chosenColor = formData.get("user-color").slice(1)
+    const chosenScheme = formData.get("color-schemes").toLowerCase()
+
+    fetch(`https://www.thecolorapi.com/scheme?hex=${chosenColor}&mode=${chosenScheme}&count=6`)
     .then(res => res.json())
     .then(data => console.log(data))
 }
 
-colorBtn.addEventListener("click", generateColorScheme)
+form.addEventListener("submit", generateColorScheme)
