a9dd71d7f805442ca65137dfef9dcda91272aa45
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2025-10-15 22:11:27 -0500

    set button to call named function for scheme generation

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const colorBtn = document.getElementById("get-color")
 
+function generateColorScheme(){
+    fetch("https://www.thecolorapi.com/scheme?hex=24B1E0&mode=triad&count=6")
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
 
-colorBtn.addEventListener("click", ()=>{
-
-fetch("https://www.thecolorapi.com")
-.then(res => res.json())
-.then(data => console.log(data))        
-
-})
+colorBtn.addEventListener("click", generateColorScheme)
