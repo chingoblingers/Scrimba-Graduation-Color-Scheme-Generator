200798592d20dc1981edb88d885061d487e27d80
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2025-10-16 21:03:14 -0500

    added color names and improved css layout.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function generateColorScheme(e){
     const chosenColor = formData.get("user-color").slice(1)
     const chosenScheme = formData.get("color-schemes")
 
-    fetch(`https://www.thecolorapi.com/scheme?hex=${chosenColor}&mode=${chosenScheme}&count=6`)
+    fetch(`https://www.thecolorapi.com/scheme?hex=${chosenColor}&mode=${chosenScheme}&count=5`)
     .then(res => res.json())
     .then(data => renderColorList(data.colors))
 }
@@ -17,8 +17,11 @@ form.addEventListener("submit", generateColorScheme)
 
 function generateColorListHtml(arr){
     return arr.map(color => {
-     return  ` <div class="color-holder" style="background-color: ${color.hex.value};">  
-       </div> ` 
+     return  ` <div class="color-container">
+                <div class="color-holder" style="background-color: ${color.hex.value};"> </div> 
+                <p class="color-name"> ${color.hex.value} </p>
+                </div>
+                ` 
     }).join(" ")
 
 
