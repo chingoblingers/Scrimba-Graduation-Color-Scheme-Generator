a3be8860f3bbf9f9089cca8c4e070d30545b76ab
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2025-10-16 20:15:24 -0500

    completed api call function

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const form = document.getElementById("color-form")
+const colorContainer = document.getElementById("color-list")
+let colorBox  = []
 
 function generateColorScheme(e){
     e.preventDefault()
     const formData = new FormData(form)
     const chosenColor = formData.get("user-color").slice(1)
-    const chosenScheme = formData.get("color-schemes").toLowerCase()
+    const chosenScheme = formData.get("color-schemes")
 
     fetch(`https://www.thecolorapi.com/scheme?hex=${chosenColor}&mode=${chosenScheme}&count=6`)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => renderColorList(data.colors))
 }
 
 form.addEventListener("submit", generateColorScheme)
+
+function generateColorListHtml(arr){
+    return arr.map(color => {
+     return  ` <div class="color-holder" style="background-color: ${color.hex.value};">  
+       </div> ` 
+    }).join(" ")
+
+
+}
+
+function renderColorList(arr){
+    const list = generateColorListHtml(arr)
+    colorContainer.innerHTML = list
+}
